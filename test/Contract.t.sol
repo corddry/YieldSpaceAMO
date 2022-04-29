e442d5d2b7c051abebfaa4fd2c960afe84c0520b
Richie <devtooligan@gmail.com>
2022-04-29 15:17:59 -0700

    fix: remove extranneous comments

--- test/Contract.t.sol
+++ test/Contract.t.sol
@@ -3,33 +3,10 @@ pragma solidity 0.8.13;
 
 import "forge-std/Test.sol";
 
-contract ContractTest is DSTest {
+contract YieldSpaceAMOTest is Test {
     function setUp() public {}
 
     function testExample() public {
         assertTrue(true);
     }
 }
-// commit f71a5703af2f01774f882d741652c087783197b7 (HEAD -> refactor/views-style)
-// Author: Richie <devtooligan@gmail.com>
-// Date:   Fri Apr 29 11:09:36 2022 -0700
-
-//     deleteme
-
-// commit 7eb670d9bc17f230c82bd806ee5f433a0ce44e85
-// Author: Richie <devtooligan@gmail.com>
-// Date:   Fri Apr 29 10:28:34 2022 -0700
-
-//     forge install: forge-std
-
-// commit 32b73403280205080de6114c4b3bc767ae99af79
-// Author: Richie <devtooligan@gmail.com>
-// Date:   Fri Apr 29 10:26:41 2022 -0700
-
-//     fix: use camel
-
-// commit 7e7b415394dbdb3e74134a7c8273f8905fd37959 (origin/refactor/views-style)
-// Author: alcueca <alberto@yield.is>
-// Date:   Fri Apr 29 16:20:45 2022 +0100
-
-//     removed footguns
