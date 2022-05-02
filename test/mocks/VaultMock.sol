ad67b8c2bf44a3625b40f65654aa494276b6d799
alcueca <alberto@yield.is>
2022-05-02 10:53:29 +0100

    tweak: refactor VaultMock

--- test/mocks/VaultMock.sol
+++ test/mocks/VaultMock.sol
@@ -1,22 +1,13 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.13;
 import "../../lib/yield-utils-v2/contracts/token/IERC20.sol";
+import "../../lib/vault-interfaces/src/ICauldron.sol";
 import "../../lib/vault-interfaces/src/IFYToken.sol";
 import "../../lib/vault-interfaces/src/DataTypes.sol";
 import "../../lib/yieldspace-interfaces/IPool.sol";
 import "./BaseMock.sol";
 import "./FYTokenMock.sol";
 
-
-interface ILadle {
-    function pour(bytes12 vaultId_, address to, int128 ink, int128 art) external payable;
-    function build(bytes6 seriesId, bytes6 ilkId, uint8 salt) external returns (bytes12 vaultId, DataTypes.Vault memory vault);
-    function cauldron() external view returns (ICauldron);
-}
-
-interface ICauldron {
-    function balances(bytes12 vault) external view returns (DataTypes.Balances memory);
-}
 
 library CauldronMath {
     /// @dev Add a number (which might be negative) to a positive, and revert if the result is negative.
@@ -26,57 +17,60 @@ library CauldronMath {
     }
 }
 
-contract VaultMock is ICauldron, ILadle {
+
+/// @notice Simplified mock of the Yield Protocol for a single asset.
+/// This contract is all the Cauldron, Ladle and Join.
+contract VaultMock {
     using CauldronMath for uint128;
 
-    ICauldron public immutable override cauldron;
+    ICauldron public immutable cauldron;
     BaseMock public immutable base;
-    address public immutable baseJoin;  // = address(this)
-    bytes6 public immutable baseId;      // = bytes6(1)
 
     mapping (bytes6 => DataTypes.Series) public series;
     mapping (bytes12 => DataTypes.Vault) public vaults;
-    mapping (bytes12 => DataTypes.Balances) internal _balances;
+    mapping (bytes12 => DataTypes.Balances) public balances;
+    mapping (bytes6 => IPool) public pools;
 
-    function balances(bytes12 vault) external view returns (DataTypes.Balances memory) {
-        return _balances[vault];
-    }
     uint96 public lastVaultId;
-    uint48 public nextSeriesId;
 
     constructor() {
         cauldron = ICauldron(address(this));
         base = new BaseMock();
-        baseJoin = address(this);
-        baseId = bytes6(uint48(1));
     }
 
-    function addSeries(uint32 maturity_) external returns (bytes6) {
+    /// @notice Generate a new series and fyToken for the base at the given maturity.
+    function addSeries(bytes6 seriesId, uint32 maturity_) external {
         IFYToken fyToken = IFYToken(address(new FYTokenMock(base, maturity_)));
-        series[bytes6(nextSeriesId++)] = DataTypes.Series({
+        series[bytes6(seriesId)] = DataTypes.Series({
             fyToken: fyToken,
             maturity: maturity_,
-            baseId: baseId
+            baseId: 0x000000000000
         });
-
-        return bytes6(nextSeriesId - 1);
     }
 
-    function build(bytes6 seriesId, bytes6 ilkId, uint8) external override returns (bytes12 vaultId, DataTypes.Vault memory vault) {
+    /// @notice Add a matching pool for a series.
+    function addPool(bytes6 seriesId, IPool pool) external {
+        require (series[seriesId].fyToken == pool.fyToken(), "Mismatched fyToken");
+        pools[seriesId] = pool;
+    }
+
+    /// @notice Create a vault for a given series
+    function build(bytes6 seriesId, bytes6, uint8) external returns (bytes12 vaultId, DataTypes.Vault memory vault) {
         vaults[bytes12(lastVaultId++)] = DataTypes.Vault({
             owner: msg.sender,
             seriesId: seriesId,
-            ilkId: ilkId
+            ilkId: 0x00000000
         });
 
         return (bytes12(lastVaultId - 1), vaults[bytes12(lastVaultId - 1)]);
     }
 
-    function pour(bytes12 vaultId, address to, int128 ink, int128 art) external payable override {
+    /// @notice Borrow or repay, with no collateralization checks, and minting or burning base and fyToken instead of transferring.
+    function pour(bytes12 vaultId, address to, int128 ink, int128 art) external payable {
         if (ink > 0) base.burn(address(this), uint128(ink)); // Simulate taking the base, which is also the collateral
         if (ink < 0) base.mint(to, uint128(-ink));
-        _balances[vaultId].ink = _balances[vaultId].ink.add(ink);
-        _balances[vaultId].art = _balances[vaultId].art.add(art);
+        balances[vaultId].ink = balances[vaultId].ink.add(ink);
+        balances[vaultId].art = balances[vaultId].art.add(art);
         address fyToken = address(series[vaults[vaultId].seriesId].fyToken);
         if (art > 0) FYTokenMock(fyToken).mint(to, uint128(art));
         if (art < 0) FYTokenMock(fyToken).burn(fyToken, uint128(-art));
