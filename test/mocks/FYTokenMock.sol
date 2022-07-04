ff3fb92f3f84d9b288f5fab19503fc873850e920
alcueca <alberto@yield.is>
2022-07-04 09:31:46 +0100

    Fixed mock, tests now break

--- test/mocks/FYTokenMock.sol
+++ test/mocks/FYTokenMock.sol
@@ -27,6 +27,25 @@ contract FYTokenMock is ERC20Permit {
         _burn(from, amount);
     }
 
+        /// @dev Burn fyTokens.
+    /// Any tokens locked in this contract will be burned first and subtracted from the amount to burn from the user's wallet.
+    /// This feature allows someone to transfer fyToken to this contract to enable a `burn`, potentially saving the cost of `approve` or `permit`.
+    function _burn(address from, uint256 amount) internal override returns (bool) {
+        // First use any tokens locked in this contract
+        uint256 available = _balanceOf[address(this)];
+        if (available >= amount) {
+            return super._burn(address(this), amount);
+        } else {
+            if (available > 0) super._burn(address(this), available);
+            unchecked {
+                _decreaseAllowance(from, amount - available);
+            }
+            unchecked {
+                return super._burn(from, amount - available);
+            }
+        }
+    }
+
     function mintWithUnderlying(address to, uint256 amount) public {
         _mint(to, amount); // It would be neat to check that the underlying has been sent to the join
     }
